21cf1724979f2d7d0d08ae2da52d165e77b17709
shi-ya418 <takuya.shiiya@gmail.com>
2020-07-20 16:54:47 +0900

    new

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,6 +11,9 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
 
     if @message.save
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
