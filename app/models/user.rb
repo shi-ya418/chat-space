e96ce50196bce2cec9a3039a5bd50d403e40f7b5
shi-ya418 <takuya.shiiya@gmail.com>
2020-07-21 18:05:29 +0900

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, password_length: 8..128
+         :recoverable, :rememberable, :validatable
 
   has_many :group_users
   has_many :groups, through: :group_users
